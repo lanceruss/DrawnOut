c57caa6f8ba80358ece984eec283e80044de10c3
Lance Russ <lance.r.russ@gmail.com>
2017-02-05 17:52:10 -0600

    upload changes

--- Fictionary/Profile2ViewController.swift
+++ Fictionary/Profile2ViewController.swift
@@ -26,17 +26,20 @@ class Profile2ViewController: UIViewController, UICollectionViewDataSource, UICo
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //view
         self.view.backgroundColor = UIColor.shamrock()
         
+        //header properties
         bigHeader.backgroundColor = UIColor.pastelGreen()
-        
         bigHeader.layer.shadowColor = UIColor.blackColor().CGColor
         bigHeader.layer.shadowOpacity = 0.25
         bigHeader.layer.shadowOffset = CGSize(width: 0, height: 1)
         bigHeader.layer.shadowRadius = 3.5
         
+        //collection view
         collectionView.backgroundColor = UIColor.shamrock()
         
+        //back button view
         backButtonView.backgroundColor = UIColor.shamrock()
         backButtonView.layer.cornerRadius = 0.5 * backButtonView.bounds.size.height
         
@@ -111,7 +114,6 @@ class Profile2ViewController: UIViewController, UICollectionViewDataSource, UICo
         userImageFilename.dataWithMaxSize(3 * 1024 * 1024) { (data, error) -> Void in
             
             if (error != nil) {
-                // Uh-oh, an error occurred!
                 print(error)
             } else {
                 // Data for "images/island.jpg" is returned
@@ -122,12 +124,9 @@ class Profile2ViewController: UIViewController, UICollectionViewDataSource, UICo
                     
                 })
             }
-            
-            
             //self.cardTableView.reloadData()
         }
         
-
         cell.cardImageView.image = UIImage(named: self.imageFilenames[indexPath.row])
         cell.cardImageView.layer.shadowOpacity = 0.3
         cell.cardImageView.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
@@ -137,18 +136,17 @@ class Profile2ViewController: UIViewController, UICollectionViewDataSource, UICo
         cell.activityIndicator.hidden = true
 
         return cell
-        
-        
-        
     }
     
     func collectionView(collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                                sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        //device screen size
+        
+        /* device screen size */
         let width = UIScreen.mainScreen().bounds.size.width
         let height = UIScreen.mainScreen().bounds.size.height
-        //calculation of cell size
+        
+        /* calculate and return cell size */
         return CGSize(width: ((width / 2) - 15), height: (height / 2.5) - 10)
     }
     
